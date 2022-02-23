64668e65e5c8113609a97e34251edd06795adf57
楚杰 周 <zhouchujiede@126.com>
2022-02-23 15:41:39 +0800

    233

--- main.c
+++ main.c
@@ -1,19 +1,20 @@
-#include<stdio.h>
+ï»¿#include<stdio.h>
 #include<windows.h>
 #include<string.h>
 int count = 1;
+char keyword[20];
 
-struct Score//Ò»¸ö½á¹¹Ìå
+struct Score//ä¸€ä¸ªç»“æ„ä½“
 {
-	char num[12];  	//Ñ§ºÅ 
-	char name[10];  //ĞÕÃû 
-	int cpp;		//C³É¼¨ 
-	int math;		//¸ßÊı³É¼¨ 
-	int english;		//Ó¢Óï³É¼¨ 
-	int sum; //Èı¿Æ³É¼¨×Ü·Ö
-}stu[100];
+	int num;  	//å­¦å· 
+	char name[10];  //å§“å 
+	int cpp;		//Cæˆç»© 
+	int math;		//é«˜æ•°æˆç»© 
+	int english;		//è‹±è¯­æˆç»© 
+	int sum; //ä¸‰ç§‘æˆç»©æ€»åˆ†
+}stu[101];
 
-void  SetPos(int  x, int  y)//ÉèÖÃ¹â±êÎ»ÖÃ
+void  SetPos(int  x, int  y)//è®¾ç½®å…‰æ ‡ä½ç½®
 {
 
 	COORD  point = { x ,  y };
@@ -21,7 +22,7 @@ void  SetPos(int  x, int  y)//ÉèÖÃ¹â±êÎ»ÖÃ
 	SetConsoleCursorPosition(HOutput, point);
 }
 
-void error()//ÉÁºì
+void error()//é—ªçº¢
 {
 	system("color 41");
 	Sleep(300);
@@ -36,83 +37,83 @@ void error()//ÉÁºì
 	system("color 71");
 }
 
-void enter()//ÉÁÂÌ
+void enter()//é—ªç»¿
 {
 	system("color 21");
 	Sleep(800);
 	system("color 71");
 }
 
-void printmenu(int no)//´òÓ¡±êÌâ
+void printmenu(int no)//æ‰“å°æ ‡é¢˜
 {
 	switch (no)
 	{
 	case 0:
 		break;
-	case 1://Ö÷²Ëµ¥
-		printf("  [Ñ§Éú³É¼¨¹ÜÀíÏµÍ³]\n -------------------------------------------------\n");
+	case 1://ä¸»èœå•
+		printf("  [å­¦ç”Ÿæˆç»©ç®¡ç†ç³»ç»Ÿ]\n -------------------------------------------------\n");
 		printf("   ____ _       ______       ______       ______\n");
 		printf("  |/  ;\\ |     | ==== |     | | |  |     | + 40 |\n");
 		printf("  |\\_ _/ |     |      |     | |_|  |     | + 30 |\n");
 		printf("  |  \\\\  |     |______|     | \\ / _|     | = 70 |\n");
-		printf("  |___\\\\_|     (______(     |__¨‹_|/     |______| \n");
-		printf("   1.²éÑ¯       2.Â¼Èë       3.±à¼­       4.Í³¼Æ\n");
+		printf("  |___\\\\_|     (______(     |__â–¼_|/     |______| \n");
+		printf("   1.æŸ¥è¯¢       2.å½•å…¥       3.ç¼–è¾‘       4.ç»Ÿè®¡\n");
 		printf("\n");
 		printf("   ______       _____        ______       ______\n");
 		printf("  |list  |     |||_||\\      | ABOUT|     |      |\n");
 		printf("  |======|     ||___| |     |\\US   |     | EXIT |\n");
-		printf("  |======|     |      |     |\\\\ :) |     |  -¡ú |\n");
+		printf("  |======|     |      |     |\\\\ :) |     |  -â†’ |\n");
 		printf("  |______|     |______|     |_\\\\___|     |______|\n");
-		printf("   5.ÏÔÊ¾       6.´æÈ¡       7.¹ØÓÚ       8.ÍË³ö\n");
-		printf(" -------------------------------------------------\n  [Ñ¡Ôñ] Êı×Ö»ò·½Ïò¼ü    [È·¶¨] »Ø³µ\n");
+		printf("   5.æ˜¾ç¤º       6.å­˜å–       7.å…³äº       8.é€€å‡º\n");
+		printf(" -------------------------------------------------\n  [é€‰æ‹©] æ•°å­—æˆ–æ–¹å‘é”®    [ç¡®å®š] å›è½¦\n");
 		break;
-	case 2://ÃÜÂë½çÃæ
-		printf("  [ÇëÊäÈëÃÜÂë]\n -------------------------------------------------\n");
+	case 2://å¯†ç ç•Œé¢
+		printf("  [è¯·è¾“å…¥å¯†ç ]\n -------------------------------------------------\n");
 		printf("\n\n\n\n\n\n");
-		printf("  ÃÜÂë[                                      ]\n");
+		printf("  å¯†ç [                                      ]\n");
 		printf("\n\n\n\n\n\n");
-		printf(" -------------------------------------------------\n  [ÊäÈë] Êı×Ö¼ü    [È·¶¨] »Ø³µ\n");
+		printf(" -------------------------------------------------\n  [è¾“å…¥] æ•°å­—é”®    [ç¡®å®š] å›è½¦\n");
 		break;
-	case 3://²éÑ¯²Ëµ¥
-		printf("  [Ñ§Éú³É¼¨²éÑ¯]\n ---------------------------------------------------\n");
-		printf("   ______       ______       ______\n");
-		printf("  |      |     |      |     |      |\n");
-		printf("  |      |     |      |     | EXIT |\n");
-		printf("  | NAME |     | CODE |     | -->  |\n");
-		printf("  |______|     |______|     |______|\n");
-		printf("   1.Ãû×Ö       2.Ñ§ºÅ       3.ÍË³ö\n");
+	case 3://æŸ¥è¯¢èœå•
+		printf("  [å­¦ç”Ÿæˆç»©æŸ¥è¯¢]\n -------------------------------------------------\n");
+		printf("    __________________   ____   ____       ____\n");
+		printf("  |                    | æœç´¢ | ç¼–è¾‘ |   | é€€å‡º |\n");
+		printf("    ------------------   ----   ----       ----\n");
+		printf("\n   å§“åï¼š");
 		printf("\n");
+		printf("\n   å­¦å·ï¼š");
 		printf("\n");
+		printf("\n  Cæˆç»©ï¼š");
 		printf("\n");
+		printf("\n   æ•°å­¦ï¼š");
 		printf("\n");
+		printf("\n   è‹±è¯­ï¼š          æ€»åˆ†ï¼š");
 		printf("\n");
-		printf("\n");
-		printf("\n");
-		printf(" -------------------------------------------------\n  [Ñ¡Ôñ] Êı×Ö»ò·½Ïò¼ü    [È·¶¨] »Ø³µ\n");
+		printf(" -------------------------------------------------\n  [é€‰æ‹©] æ•°å­—æˆ–æ–¹å‘é”®    [ç¡®å®š] å›è½¦\n");
 		break;
-	case 4://Â¼Èë²Ëµ¥
-		printf("  [Ñ§Éú³É¼¨Â¼Èë]\n --------------------------------------------------\n");
-		printf("\n   ĞÕÃû£º");
+	case 4://å½•å…¥èœå•
+		printf("  [å­¦ç”Ÿæˆç»©å½•å…¥]\n --------------------------------------------------\n");
+		printf("\n   å§“åï¼š");
 		printf("\n");
-		printf("\n   Ñ§ºÅ£º");
+		printf("\n   å­¦å·ï¼š");
 		printf("\n");
-		printf("\n  C³É¼¨£º");
+		printf("\n  Cæˆç»©ï¼š");
 		printf("\n");
-		printf("\n   ÊıÑ§£º");
+		printf("\n   æ•°å­¦ï¼š");
 		printf("\n");
-		printf("\n   Ó¢Óï£º");
+		printf("\n   è‹±è¯­ï¼š");
 		printf("\n");
-		printf("\n   ±£´æ²¢¼ÌĞø    ±£´æ²¢ÍË³ö");
+		printf("\n   ä¿å­˜å¹¶ç»§ç»­    ä¿å­˜å¹¶é€€å‡º");
 		printf("\n");
 		printf("\n");
-		printf(" -------------------------------------------------\n  [Ñ¡Ôñ] Êı×Ö»ò·½Ïò¼ü    [È·¶¨] »Ø³µ\n");
+		printf(" -------------------------------------------------\n  [é€‰æ‹©] æ–¹å‘é”®    [ç¡®å®š] å›è½¦\n");
 		break;
-	case 5://»¶Ó­½çÃæ
-		printf("  [Ñ§Éú³É¼¨¹ÜÀíÏµÍ³]\n ---------------------------------------------------\n");
+	case 5://æ¬¢è¿ç•Œé¢
+		printf("  [å­¦ç”Ÿæˆç»©ç®¡ç†ç³»ç»Ÿ]\n -------------------------------------------------\n");
 		printf("\n");
 		printf("\n\t\t###############");
-		printf("\n\t\t# Ñ§ Éú ³É ¼¨ #");
-		printf("\n\t\t# ¹Ü Àí Ïµ Í³ #");
+		printf("\n\t\t# å­¦ ç”Ÿ æˆ ç»© #");
+		printf("\n\t\t# ç®¡ ç† ç³» ç»Ÿ #");
 		printf("\n\t\t###############");
 		printf("\n");
 		printf("\n");
@@ -122,7 +123,7 @@ void printmenu(int no)//´òÓ¡±êÌâ
 		printf("\n");
 		printf("\n");
 		printf("\n");
-		printf(" -------------------------------------------------\n  [È·¶¨] »Ø³µ\n");
+		printf(" -------------------------------------------------\n  [ç¡®å®š] å›è½¦\n");
 		break;
 
 	default:
@@ -131,21 +132,21 @@ void printmenu(int no)//´òÓ¡±êÌâ
 
 }
 
-void printbox(int x, int y, int a, int b)//´òÓ¡¸ö¿ò£¬xyÃªµã×ø±ê£¬ab¿òµÄ³¤¿í
+void printbox(int x, int y, int a, int b)//æ‰“å°ä¸ªæ¡†ï¼Œxyé”šç‚¹åæ ‡ï¼Œabæ¡†çš„é•¿å®½
 {
 	x -= 1; y -= 1; a -= 2; b -= 1;
 	SetPos(x, y);
-	printf("¨");
+	printf("â—¤");
 	SetPos(x + a, y);
-	printf("¨");
+	printf("â—¥");
 	SetPos(x, y + b);
-	printf("¨");
+	printf("â—£");
 	SetPos(x + a, y + b);
-	printf("¨");
+	printf("â—¢");
 
 }
 
-void printspace(int x, int y, int a, int b)//´òÓ¡¸ö¿ò£¬xyÃªµã×ø±ê£¬ab¿òµÄ³¤¿í
+void printspace(int x, int y, int a, int b)//æ‰“å°ä¸ªæ¡†ï¼Œxyé”šç‚¹åæ ‡ï¼Œabæ¡†çš„é•¿å®½
 {
 	x -= 1; y -= 1; a -= 2; b -= 1;
 	SetPos(x, y);
@@ -159,9 +160,9 @@ void printspace(int x, int y, int a, int b)//´òÓ¡¸ö¿ò£¬xyÃªµã×ø±ê£¬ab¿òµÄ³¤¿í
 
 }
 
-void password()//ÃÜÂëÈ·ÈÏ
+void password()//å¯†ç ç¡®è®¤
 {
-	int password = 02;//ÃÜÂë
+	int password = 02;//å¯†ç 
 	int scan = 0;
 	while (1)
 	{
@@ -173,23 +174,23 @@ void password()//ÃÜÂëÈ·ÈÏ
 		{
 			enter();
 			system("cls");
-			break;//ÔËĞĞµ½ÕâÀï´ú±íÃÜÂëÕıÈ·
+			break;//è¿è¡Œåˆ°è¿™é‡Œä»£è¡¨å¯†ç æ­£ç¡®
 		}
 		error();
 	}
 
 }
 
-int mainmenu()//Ö÷²Ëµ¥½çÃæ
+int mainmenu()//ä¸»èœå•ç•Œé¢
 {
 	printmenu(1);
 	printbox(2, 3, 10, 6);
 	SetPos(1, 17);
-	int code = 1, ch, check = 0;//codeÖ¸¹¦ÄÜ´úÂë
-	while (check == 0)//ÓÃ·½Ïò¼üÑ¡Ôñ¹¦ÄÜ
+	int code = 1, ch, check = 0;//codeæŒ‡åŠŸèƒ½ä»£ç 
+	while (check == 0)//ç”¨æ–¹å‘é”®é€‰æ‹©åŠŸèƒ½
 	{
 		ch = getch();
-		switch (code)//Ä¨µô¿ò¿ò
+		switch (code)//æŠ¹æ‰æ¡†æ¡†
 		{
 		case 1:
 			printspace(2, 3, 10, 6);
@@ -243,7 +244,7 @@ int mainmenu()//Ö÷²Ëµ¥½çÃæ
 				code -= 4;
 			code += 1;
 			break;
-		case 49://Êı×Ö¼ü
+		case 49://æ•°å­—é”®
 			code = 1;
 			break;
 		case 50:
@@ -267,7 +268,7 @@ int mainmenu()//Ö÷²Ëµ¥½çÃæ
 		case 56:
 			code = 8;
 			break;
-		case 97://²¿·Ö¼üÅÌÓĞÁ½ÖÖÊı×Ö¼ü
+		case 97://éƒ¨åˆ†é”®ç›˜æœ‰ä¸¤ç§æ•°å­—é”®
 			code = 1;
 			break;
 		case 98:
@@ -298,7 +299,7 @@ int mainmenu()//Ö÷²Ëµ¥½çÃæ
 			break;
 		}
 
-		switch (code)//´òÓ¡¿ò¿ò
+		switch (code)//æ‰“å°æ¡†æ¡†
 		{
 		case 1:
 			printbox(2, 3, 10, 6);
@@ -334,26 +335,73 @@ int mainmenu()//Ö÷²Ëµ¥½çÃæ
 	return code;
 }
 
+int search2(int code)
+{
+	int num = 0;
+	int comper = 1;
+
+	switch (code)
+	{
+	case 1:
+		SetPos(3, 3); printf("               ");
+		SetPos(3, 3);
+		gets(keyword);
+		return 0;
+	case 2:
+		if (keyword[0] >= '0' && keyword[0] <= '9')
+			num = atoi(keyword);
+		else
+		{
+			while (comper)
+			{
+				if (strcmp(keyword, stu[num].name) == 0)
+				    comper = 0;
+				else
+				    num += 1;
+			}
+		}
+		SetPos(9, 6); printf("                ");
+		SetPos(9, 8); printf("                ");
+		SetPos(9, 10); printf("                ");
+		SetPos(9, 12); printf("                ");
+		SetPos(9, 14); printf("                ");
+		SetPos(9, 6);puts(stu[num].name);
+		SetPos(9, 8);printf("%d",stu[num].num);
+		SetPos(9, 10);printf("%d",stu[num].cpp);
+		SetPos(9, 12);printf("%d", stu[num].math);
+		SetPos(9, 14);printf("%d", stu[num].english);
+		SetPos(27, 14); printf("%d", stu[num].sum);
+		return 0;
+
+	default:
+		break;
+	}
+
+}
+
 void search()
 {
 	system("cls");
 	printmenu(3);
-	printbox(2, 3, 10, 6);
+	printbox(3, 3, 22, 3);
 	SetPos(1, 17);
-	int code = 1, ch, check = 0;//codeÖ¸¹¦ÄÜ´úÂë 1°´Ãû 2°´ºÅ 3ÍË³ö
-	while (check == 0)//ÓÃ·½Ïò¼üÑ¡Ôñ¹¦ÄÜ
+	int code = 1, ch, check = 0;//codeæŒ‡åŠŸèƒ½ä»£ç  
+	while (check == 0)//ç”¨æ–¹å‘é”®é€‰æ‹©åŠŸèƒ½
 	{
 		ch = getch();
-		switch (code)//Ä¨µô¿ò¿ò
+		switch (code)//æŠ¹æ‰æ¡†æ¡†
 		{
 		case 1:
-			printspace(2, 3, 10, 6);
+			printspace(3, 3, 22, 3);
 			break;
 		case 2:
-			printspace(15, 3, 10, 6);
+			printspace(24, 3, 8, 3);
 			break;
 		case 3:
-			printspace(28, 3, 10, 6);
+			printspace(31, 3, 8, 3);
+			break;
+		case 4:
+			printspace(42, 3, 8, 3);
 			break;
 		default:
 			break;
@@ -363,65 +411,36 @@ void search()
 		{
 		case 75://left
 			if (code == 1)
-				code += 3;
+				code = 4;
 			code -= 1;
 			break;
 		case 77://right
-			if (code == 3)
-				code -= 3;
+			if (code == 4)
+				code = 1;
 			code += 1;
 			break;
-		case 49://Êı×Ö¼ü
-			code = 1;
-			break;
-		case 50:
-			code = 2;
-			break;
-		case 51:
-			code = 3;
-			break;
-		case 97://²¿·Ö¼üÅÌÓĞÁ½ÖÖÊı×Ö¼ü
-			code = 1;
-			break;
-		case 98:
-			code = 2;
-			break;
-		case 99:
-			code = 3;
-			break;
 		case 13:
-			check = 1;
+			check = search2(code);
 			break;
 		default:
 			break;
 		}
 
-		switch (code)//´òÓ¡¿ò¿ò
+		switch (code)//æ‰“å°æ¡†æ¡†
 		{
 		case 1:
-			printbox(2, 3, 10, 6);
+			printbox(3, 3, 22, 3);
 			break;
 		case 2:
-			printbox(15, 3, 10, 6);
+			printbox(24, 3, 8, 3);
 			break;
 		case 3:
-			printbox(28, 3, 10, 6);
+			printbox(31, 3, 8, 3);
 			break;
 		case 4:
-			printbox(41, 3, 10, 6);
+			printbox(42, 3, 8, 3);
 			break;
-		case 5:
-			printbox(2, 10, 10, 6);
-			break;
-		case 6:
-			printbox(15, 10, 10, 6);
-			break;
-		case 7:
-			printbox(28, 10, 10, 6);
-			break;
-		case 8:
-			printbox(41, 10, 10, 6);
-			break;
+		
 		default:
 			break;
 		}
@@ -444,31 +463,52 @@ void search()
 
 int LuRu2(int code)
 {
+	int num=0;
 	switch (code)
 	{
 	case 1:
 		SetPos(9, 3);
-		gets(stu[count].name);
+		gets(stu[100].name);
 		return 0;
 	case 2:
 		SetPos(9, 5);
-		gets(stu[count].num);
+		scanf_s("%d", &stu[100].num);
 		return 0;
 	case 3:
 		SetPos(9,7);
-		scanf_s("%d",&stu[count].cpp);
+		scanf_s("%d",&stu[100].cpp);
 		return 0;
 	case 4:
 		SetPos(9, 9);
-		scanf_s("%d", &stu[count].math);
+		scanf_s("%d", &stu[100].math);
 		return 0;
 	case 5:
 		SetPos(9, 11);
-		scanf_s("%d", &stu[count].math);
+		scanf_s("%d", &stu[100].english);
+		return 0;
+	case 6:
+		num = stu[100].num;
+		strcpy_s(stu[num].name, strlen(stu[100].name) + 1, stu[100].name);
+		stu[num].num = stu[100].num;
+		stu[num].cpp = stu[100].cpp;
+		stu[num].math = stu[100].math;
+		stu[num].english = stu[100].english;
+		stu[num].sum = stu[100].cpp + stu[100].math + stu[100].english;
+		SetPos(9, 3); printf("                ");
+		SetPos(9, 5); printf("                ");
+		SetPos(9, 7); printf("                ");
+		SetPos(9, 9); printf("                ");
+		SetPos(9, 11); printf("                ");
 		return 0;
 	case 7:
+		num = stu[100].num;
+		strcpy_s(stu[num].name,strlen(stu[100].name)+1,stu[100].name);
+		stu[num].num = stu[100].num;
+		stu[num].cpp = stu[100].cpp;
+		stu[num].math = stu[100].math;
+		stu[num].english = stu[100].english;
+		stu[num].sum = stu[100].cpp + stu[100].math + stu[100].english;
 		return 1;
-		break;
 	default:
 		break;
 	}
@@ -481,11 +521,11 @@ void LuRu()
 	printmenu(4);
 	printbox(9, 3, 20, 3);
 	SetPos(1, 17);
-	int code = 1, ch, check = 0;//codeÖ¸¹¦ÄÜ´úÂë 
-	while (check == 0)//ÓÃ·½Ïò¼üÑ¡Ôñ¹¦ÄÜ
+	int code = 1, ch, check = 0;//codeæŒ‡åŠŸèƒ½ä»£ç  
+	while (check == 0)//ç”¨æ–¹å‘é”®é€‰æ‹©åŠŸèƒ½
 	{
 		ch = getch();
-		switch (code)//Ä¨µô¿ò¿ò
+		switch (code)//æŠ¹æ‰æ¡†æ¡†
 		{
 		case 1:
 			printspace(9, 3, 20, 3);
@@ -516,13 +556,27 @@ void LuRu()
 		{
         case 72://up
 			if (code == 1)
-				code = 7;
+				code = 6;
+			else if (code == 7)
+				code = 5;
 			else
 				code -= 1;
 			break;
 		case 80://down
-			if (code == 7)
+			if (code >= 6)
 				code = 1;
+			else
+				code += 1;
+			break;
+		case 75://left
+			if (code ==6)
+				code = 7;
+			else
+			    code -= 1;
+			break;
+		case 77://right
+			if (code == 7)
+				code = 6;
 			else
 				code += 1;
 			break;
@@ -532,7 +586,7 @@ void LuRu()
 			break;
 		}
 
-		switch (code)//´òÓ¡¿ò¿ò
+		switch (code)//æ‰“å°æ¡†æ¡†
 		{
 		case 1:
 			printbox(9, 3, 20, 3);
@@ -568,7 +622,7 @@ int main()
 {
 	system("mode con cols=51 lines=18");
 	system("color 71");
-	system("title Ñ§Éú³É¼¨¹ÜÀíÏµÍ³");//ÒÔÉÏ¸Ä±ä´°¿ÚÑÕÉ« ´óĞ¡ ±êÌâ
+	system("title å­¦ç”Ÿæˆç»©ç®¡ç†ç³»ç»Ÿ");//ä»¥ä¸Šæ”¹å˜çª—å£é¢œè‰² å¤§å° æ ‡é¢˜
 	printmenu(5);
 	getch();
 	password();
@@ -582,7 +636,7 @@ int main()
 	while (code != 8)
 	{
      
-		code = mainmenu();//1²éÑ¯ 2Â¼Èë 3±à¼­ 4Í³¼Æ 5ÏÔÊ¾ 6´æÈ¡ 7¹ØÓÚ 8ÍË³ö
+		code = mainmenu();//1æŸ¥è¯¢ 2å½•å…¥ 3ç¼–è¾‘ 4ç»Ÿè®¡ 5æ˜¾ç¤º 6å­˜å– 7å…³äº 8é€€å‡º
 		switch (code)
 		{
 		case 1:
